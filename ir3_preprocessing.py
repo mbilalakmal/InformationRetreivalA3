e143849a3ed42a595ab8e134d2f4571c82e45e5c
Bilal Akmal <bilalakmal@hotmail.com>
2020-05-16 09:38:33 +0500

    preprocessing module added with tokenize, casefold, stopwords removal, and lemmatize functions

---
+++ ir3_preprocessing.py
@@ -0,0 +1,46 @@
+"""
+This module defines preprocessing methods for
+text documents. The steps are mentioned below:
+1. Tokenize
+2. Casefold
+3. Remove stopwords
+3. Lemmatize
+
+
+(C) 2020 Muhammad Bilal Akmal, 17K-3669
+"""
+
+import re
+
+from nltk.stem import WordNetLemmatizer
+
+
+def extract_features(document: str):
+    tokens = _get_tokens(document)
+
+    tokens = [token.lower() for token in tokens]
+
+    tokens = _remove_stopwords(tokens)
+
+    # download('wordnet')
+    lemmatizer = WordNetLemmatizer()
+    terms = [lemmatizer.lemmatize(token) for token in tokens]
+
+    return terms
+
+
+def _get_tokens(document: str):
+    pattern = r'\W+'
+    tokens = re.split(pattern, document)
+    return tokens
+
+
+def _remove_stopwords(tokens: list):
+    filename = r'stopwords.txt'
+    with open(filename, 'r') as txt_file:
+        stopwords = set(txt_file.read().split())
+
+    tokens = [
+        token for token in tokens if (token not in stopwords and len(token) > 1)
+    ]
+    return tokens
