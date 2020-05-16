6b39d008d1efabc88e71c33efcd1b47193c5ac46
Bilal Akmal <bilalakmal@hotmail.com>
2020-05-16 09:37:20 +0500

    Document class defined containing text and label

---
+++ ir3_document.py
@@ -0,0 +1,25 @@
+from ir3_preprocessing import extract_features
+
+
+class Document:
+    def __init__(
+            self,
+            doc_id: str,
+            path: str,
+            body: str,
+            category: str
+    ):
+        self.doc_id = doc_id
+        self.path = path
+        self.body = body
+        self.category = category
+        self.terms = extract_features(body)
+
+    def __repr__(self):
+        return (
+            f'Doc ID: {self.doc_id}\n'
+            f'Path: {self.path}\n'
+            # f'Title: {self.title}\n'
+            f'Body: {self.body[:30]}\n'
+            f'Category: {self.category}\n'
+        )
