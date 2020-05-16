f19dafb32cbb39cbf20c60fe5439a6c10a972447
Bilal Akmal <bilalakmal@hotmail.com>
2020-05-17 01:24:01 +0500

    Added filing for vsm_index

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from time import time
 
 from ir3_classification import evaluate_classification
-from ir3_filing import read_documents
+from ir3_filing import read_documents, load_object, store_object
 from ir3_vsm_indexer import create_vsm_matrix
 
 
 def main():
-    root_path = root_path = r'G:\My Drive\University\Spring (2020)\Information Retrieval\Assignment3\bbcsport'
+    root_path = r'G:\My Drive\University\Spring (2020)\Information Retrieval\Assignment3\bbcsport'
+    index_path = r'vsm_index'
 
     aa = time()
 
-    documents = read_documents(root_path)
-    bb = time()
+    vsm_index = load_object(index_path)
 
-    vsm_matrix, response_vector = create_vsm_matrix(documents)
+    # If index is not stored, create it and store
+    if vsm_index is None:
+        documents = read_documents(root_path)
+
+        vsm_index = create_vsm_matrix(documents)
+
+        store_object(vsm_index, index_path)
+
+    vsm_matrix, response_vector = vsm_index
+
     cc = time()
 
     k = 3
@@ -22,8 +31,7 @@ def main():
     accuracy = evaluate_classification(vsm_matrix, response_vector, k, ratio)
     dd = time()
 
-    print(f'Read in : {bb - aa} seconds\n'
-          f'VSM created in : {cc - bb} seconds\n'
+    print(f'VSM created in : {cc - aa} seconds\n'
           f'Evaluated in : {dd - cc} seconds\n')
 
     print(accuracy * 100)
